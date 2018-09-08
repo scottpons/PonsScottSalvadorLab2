f2ef5b12118e97ca35c642ac8a5111b563b612d4
scottpons <scottpons09@gmail.com>
2018-09-08 09:59:55 +0800

    Initial commit

---
+++ app/src/main/java/scottsalvador/pons/com/ponsscottsalvadorlab2/LayoutActivity.java
@@ -0,0 +1,13 @@
+package scottsalvador.pons.com.ponsscottsalvadorlab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear);
+    }
+}
